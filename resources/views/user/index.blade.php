13a7055cf605c940806708aa252bde6dd1ce049f
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2024-05-18 15:10:53 +0700

    update index penduduk

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,20 +3,18 @@
 @section('content')
     @php
         $data = [
-            'name' => 'Muhamad Edo Alfiansyah',
-            'nik' => '3505050505000',
-            'status' => 'Pendatang',
+            'name' => 'Bambang Setai Kawan Boy',
+            'nik' => '1234567891234567',
+            'status' => 'Penduduk',
             'address' => 'RT 2 RW 2 no.12, Ds.Arjosari, Kec.Blimbing, Kab. Malang',
-            'message' => 'Kulo tiang grapyak, monggo pinarak!'
         ];
     @endphp
     <div class="container">
-        <div class="avatar"></div>
-        <h1>{{ $data['name'] }}</h1>
+        <div class="avatar mb-3"></div>
+        <h2>{{ $data['name'] }}</h2>
         <p>{{ $data['nik'] }}</p>
         <p>{{ $data['status'] }}</p>
         <p>{{ $data['address'] }}</p>
-        <p>{{ $data['message'] }}</p>
     </div>
 @endsection
 
