f996e904238695d40ce80b257aa6cb4f7198dd66
fikuraru72 <abiyyufiqlal72.com>
2024-05-30 20:22:52 +0700

    repair migration

--- database/seeders/PengaduanTableSeeder.php
+++ database/seeders/PengaduanTableSeeder.php
@@ -19,14 +19,14 @@ class PengaduanTableSeeder extends Seeder
                 'nik' => '1234567890123456',
                 'pesan' => 'Lampu jalan di depan rumah saya rusak.',
                 'rt' => '01',
-                'id_user' => 1,
+
             ],
             [
                 'nama' => 'Jane Smith',
                 'nik' => '2345678901234567',
                 'pesan' => 'Ada jalan berlubang di sekitar komplek.',
                 'rt' => '02',
-                'id_user' => 2,
+
             ],
         ]);
     }
