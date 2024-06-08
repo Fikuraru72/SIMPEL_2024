6dee7ecf4d0461bd41c7fa8a867fb43290641947
fikuraru72 <abiyyufiqlal72.com>
2024-06-08 21:45:04 +0700

    refactor

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -81,7 +81,7 @@
     <!-- inject:js -->
     {{-- <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script> --}}
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-    {{-- <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script> --}}
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script src="{{ asset('template/assets/js/off-canvas.js') }}"></script>
     <script src="{{ asset('template/assets/js/hoverable-collapse.js') }}"></script>
     <script src="{{ asset('template/assets/js/template.js') }}"></script>
