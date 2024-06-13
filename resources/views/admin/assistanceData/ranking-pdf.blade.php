b27191ddfd42652db089b150457aed0057d5ea01
fikuraru72 <abiyyufiqlal72.com>
2024-06-13 11:38:05 +0700

    refactor view logi & tamplate ranking.pdf

--- resources/views/admin/assistanceData/ranking-pdf.blade.php
+++ resources/views/admin/assistanceData/ranking-pdf.blade.php
@@ -1,26 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Penerima Bansos</title>
     <style>
+        body {
+            font-family: Arial, sans-serif;
+            position: relative;
+            margin: 0;
+            padding-bottom: 150px;
+            /* Adjust this value if necessary */
+        }
+
         table {
             width: 100%;
             border-collapse: collapse;
         }
-        th, td {
+
+        th,
+        td {
             border: 1px solid #000;
             padding: 8px;
             text-align: left;
         }
+
         th {
             background-color: #f2f2f2;
         }
+
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+
+        .signature {
+            margin-top: 50px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .signature div {
+            width: 30%;
+            text-align: center;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .centered-h4 {
+            text-align: center;
+            margin-top: 0;
+        }
     </style>
 </head>
+
 <body>
-    <h2>Data Penerima Bansos</h2>
+    <h4 class="centered-h4">Data Penerima Bantuan Sosial</h4>
+    <h5 class="centered-h4"> RW 04 Kelurahan Jatimulyo, Kecamatan Lowokwaru, Kota Malang</h5>
+    <br><br><br>
     <div class="table-responsive">
         <table>
             <thead>
@@ -43,5 +86,21 @@
             </tbody>
         </table>
     </div>
+
+    <!-- Footer -->
+    <div class="footer">
+        <div class="signature">
+            <div>
+                <p>Malang, {{ \Carbon\Carbon::now()->format('d-m-Y') }}</p>
+                <p><strong>Ketua Rw.04</strong></p>
+                <br><br><br>
+                <p>_________________________</p>
+            </div>
+            <div>
+                <b><p>Hadi Kadarman</p></b>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,69 +5,108 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/logins/login-9/assets/css/login-9.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css">
+    <style>
+        body {
+            overflow: hidden;
+        }
+    </style>
     <title>Document</title>
 </head>
 
-<body>
-    <!-- Login 2 - Bootstrap Brain Component -->
-    <div class=" py-3 py-md-5">
+<body class="bg-primary text-white">
+    <section class="py-3 py-md-5 py-xl-8">
         <div class="container">
-            <div class="row justify-content-md-center">
-                <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
-                    <div class="bg-white p-4 p-md-5 rounded shadow-sm">
-                        <div>
-                            <h1 class="row justify-content-md-center">SIMPEL</h1>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="mb-5">
-                                    <h4>Log in</h4>
-                                </div>
+            <div class="row gy-4 align-items-center">
+                <div class="col-12 col-md-6 col-xl-7">
+                    <div class="d-flex justify-content-center text-bg-primary">
+                        <div class="col-12 col-xl-9">
+                            <img class="img-fluid rounded mb-4" loading="lazy" src="./assets/img/bsb-logo-light.svg"
+                                width="245" height="80" alt="BootstrapBrain Logo">
+                            <hr class="border-primary-subtle mb-4">
+                            <h2 class="h1 mb-4">We make digital products that drive you to stand out.</h2>
+                            <p class="lead mb-5">We write words, take photos, make videos, and interact with artificial
+                                intelligence.</p>
+                            <div class="text-endx">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"
+                                    fill="currentColor" class="bi bi-grip-horizontal" viewBox="0 0 16 16">
+                                    <path
+                                        d="M2 8a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                </svg>
                             </div>
                         </div>
-                        <form method="POST" action="{{ url('/login') }}">
-                            @csrf
-                            <div class="row gy-3 gy-md-4 overflow-hidden">
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-xl-5">
+                    <div class="card border-0 rounded-4">
+                        <div class="card-body p-3 p-md-4 p-xl-5">
+                            <div class="row">
                                 <div class="col-12">
-                                    <label for="username" class="form-label">Username <span
-                                            class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" name="username" id="username" required>
-                                    @error('username')
-                                        <small class="text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                                <div class="col-12">
-                                    <label for="password" class="form-label">Password <span
-                                            class="text-danger">*</span></label>
-                                    <input type="password" class="form-control" name="password" id="password"
-                                        value="" required>
-                                    @error('password')
-                                        <small class="text-danger">{{ $message }}</small>
-                                    @enderror
-                                </div>
-
-                                <div class="col-12">
-                                    <div class="d-grid">
-                                        <button class="btn btn-lg btn-primary" type="submit">Log in now</button>
+                                    <div class="mb-4">
+                                        <h3>Sign in</h3>
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                        <div class="row">
-                            <div class="col-12">
-                                <hr class="mt-5 mb-4 border-secondary-subtle">
-                                <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-end">
-                                    <a href="#!" class="link-secondary text-decoration-none">Forgot password</a>
+                            <form method="POST" action="{{ url('/login') }}">
+                                @csrf
+                                <div class="row gy-3 overflow-hidden">
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control"name="username" id="username"
+                                                placeholder="name@example.com" required>
+                                            @error('username')
+                                                <small class="text-danger">{{ $message }}</small>
+                                            @enderror
+                                            <label for="username" class="form-label">Username</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating mb-3">
+                                            <input type="password" class="form-control" name="password" id="password"
+                                                value="" placeholder="Password" required>
+                                            @error('password')
+                                                <small class="text-danger">{{ $message }}</small>
+                                            @enderror
+                                            <label for="password" class="form-label">Password</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="d-grid">
+                                            <button class="btn btn-primary btn-lg" type="submit">Log in now</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="row">
+                                <div class="col-12">
+                                    <div
+                                        class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-end mt-4">
+                                        <a href="#!">Forgot password</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-dark text-white py-5 my-5">
+        <div class="container">
+            <h5>About Us</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum
+                vestibulum.</p>
+        </div>
+    </footer>
+
+    <!-- Bootstrap 5 JS and dependencies (Popper.js) -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
